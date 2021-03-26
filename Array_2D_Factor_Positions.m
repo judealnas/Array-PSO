8f39277ade6dc182c1a42b440754d80fb0a9318b
SyrusVir <jude.alnas@gmail.com>
2021-03-26 01:02:59 -0500

    Simplified coordinate systems using meshgrid(); successful 3D plotting in cartesian, cylindrical, and spherical; next step is SLL calculation and phase generalization

--- Array_2D_Factor_Positions.m
+++ Array_2D_Factor_Positions.m
@@ -5,30 +5,38 @@ c = physconst('LightSpeed');
 Nx = 5; %# of elements in x-direction
 Ny = 5; %# of elements in y-direction
 
+I = ones(Nx,Ny); % Matri of element excitation amplitudes
 Ai = ones(Nx,Ny); %Feeding Coefficient
+Aiy = Aix;
 Dk = 1; %substrate relative permittivity/dielectric constant
 freq0 = 27e9; %Operating frequency of 28 GHz
 lambda = c/freq0/sqrt(Dk);
 k = 2*pi/lambda; %phase constant
 
-theta = (0:1:180); %degree angle
-phi = (0:1:360);
+% Angle domain
+theta = linspace(0,pi/2,500); 
+phi = linspace(0,2*pi,500);
+[Theta, Phi] = meshgrid(theta, phi);
 
 d = 1*lambda/2;   %uniform element spacing in meters
-
 D = (0:1:Nx-1)*d;   %array of element positions
 Dx = (0:1:Nx-1)'*d*ones(1,Ny); %cols=vector of distances from y-axis, rows=which column of elements
 Dy = ones(Nx,1)*(0:1:Ny-1)*d; %cols=which row of elements, rows= vector of distances from x-axis
 
-scan_theta_deg = 30; %scan angle of phased array
-scan_phi_deg = 45; %scan angle of phased array
+scan_theta = deg2rad(30); %scan angle of phased array
+scan_phi   = deg2rad(45); %scan angle of phased array
 
-x_cart = cos(deg2rad(phi(:)))*sin(deg2rad(theta(:)))';
-y_cart = sin(deg2rad(phi(:)))*sin(deg2rad(theta(:)))';
+%Cartesian axes
+x_cart = sin(Theta).*cos(Phi);
+y_cart = sin(Theta).*sin(Phi);
+
+% Per elment scanning angle phase shifts
+beta_x = sin(scan_theta)*cos(scan_phi);
+beta_y = sin(scan_theta)*sin(scan_phi);
 
 % Phase shifted axes used for calculation;
-Gam_x = x_cart - sin(deg2rad(scan_theta_deg))*cos(deg2rad(scan_phi_deg)); 
-Gam_y = y_cart - sin(deg2rad(scan_theta_deg))*sin(deg2rad(scan_phi_deg)); 
+Gam_x = sin(Theta).*cos(Phi); 
+Gam_y = sin(Theta).*sin(Phi); 
 
 %smallest dB value shown on rad patter; used to remove extreme nulls from data 
 rlim_low_db = -40; 
@@ -40,11 +48,15 @@ for col= 1:size(Dx,2)
     %reshape the column of Dx into a 1-1-Nx matrix
     %each element then multiplies the 2D Gam_x, creating a 3D matrix
     %Sum along the third dimension to return a 2D matrix
-    AFx = AFx + sum(reshape(Ai(:,col),1,1,[]).*exp(1i*k*reshape(Dx(:,col),1,1,[]).*Gam_x),3);
+    amplitude = reshape(Ai(:,col),1,1,[]);
+    dx = reshape(Dx(:,col),1,1,[]);
+    AFx = AFx + sum(amplitude.*exp(1i*k*dx.*(sin(Theta).*cos(Phi)-beta_x)),3);
 end
 
 for row = 1:size(Dy,1)
-    AFy = AFy + sum(reshape(Ai(row,:),1,1,[]).*exp(1i*k*reshape(Dy(row,:),1,1,[]).*Gam_y),3);
+    amplitude = reshape(Ai(row,:),1,1,[]);
+    dy = reshape(Dy(row,:),1,1,[]);
+    AFy = AFy + sum(amplitude.*exp(1i*k*dy.*(sin(Theta).*sin(Phi)-beta_y)),3);
 end
 
 AF = AFx .* AFy;
@@ -55,77 +67,37 @@ AF2 = abs(AF).^2;
 AF2_max = max(AF2,[],'all');
 AF2_norm = AF2./AF2_max;
 
-figure()
-s1 = surf(x_cart, y_cart,abs(AF_norm));
+%% Figure 1 plots spatially wihtin unit circle
+figure() 
+s1 = surf(sin(Theta).*cos(Phi), sin(Theta).*sin(Phi),abs(AF_norm));
 s1.EdgeColor = 'none';
 
-%% Converting to cylindrical
-x_cyl = cos(deg2rad(phi(:)))*theta(:)';
-y_cyl = sin(deg2rad(phi(:)))*theta(:)';
-figure()
+%% Figure 2 plots in cylindrical coordinates 
+% Radius maps to elevation scanning angle
+% Azimuthal angle maps to aimuthal scanning angle
+% Height (z) maps to abs(AF)
+
+x_cyl = cos(Phi).*Theta;
+y_cyl = sin(Phi).*Theta;
+
+cyl_ax_limits = [-1*pi/2 1*pi/2];
+figure() % Figure 2
 s2 = surf(x_cyl, y_cyl,abs(AF_norm));
 s2.EdgeColor = 'none';
-xlim([-90 90])
-ylim([-90 90])
+xlim(cyl_ax_limits)
+ylim(cyl_ax_limits)
 xlabel('Phi=0 Plane')
-ylabel('Phi=90 Plane')
+ylabel('Phi=$\pi/2$ Plane', 'Interpreter', 'latex')
 
 %% Converting to Spherical
-[x, y, z] = sph2cart(x_cart,y_cart,abs(AF_norm));
-figure()
-s3 = surf(z,y,x);
+sph_ax_lims = [-1 1];
+[x, y, z] = sph2cart(Phi,Theta,abs(AF_norm));
+figure() % Figure 3
+s3 = surf(x,y,z);
+xlim(sph_ax_lims)
+ylim(sph_ax_lims)
+% zlim([0 1])
 s3.EdgeColor = 'none';
 
-
-%% Peak Finding
-%Locate indices of peaks; remove index of main lobe; calculate max of remaining peaks
-
-%max SLL using valley finder; valleys used to find main node null points
-% ind_vall = find(islocalmin(AF2)); %find indices of valleys 
-% n = sort([ind_vall(:); ind_max(:)]); %sorted array of indices of valleys and the main peaks
-% null1 = n(find(n == ind_max(1)) + 1); %ind_max(1) = index of boresight
-% null2 = n(find(n == ind_max(2)) - 1); %ind_max(2) = index of rear boresight
-% 
-% [max_SLL1,ind_max_SLL] = max(AF2(null1:null2))
-% 
-% %max SLL using peak finder
-% ind_peak = find(islocalmax(AF2)); %find indices of peaks (detection may cause problems)
-% 
-% ind_side = setdiff(ind_peak,ind_max); %remove index of boresights; gives indices of side lobe peaks
-% 
-% [max_SLL2, ind2_max_SLL] = max(AF2(ind_side)); %ind2 = second-order index, an index to retrieve ANOTHER index
-% max_SLL2 == AF2(ind_side(ind2_max_ZSLL));
-% 
-% %% Plot
-% %data to plot located SL peaks
-% y = zeros(size(theta));
-% y(ind_side) = 1;
-% 
-% y1 = zeros(size(theta));
-% y1(ind_peak) = 1;
-% 
-% y2 = zeros(size(theta));
-% y2(ind_vall) = 1;
-% 
-% %Replacing nulls with NaN to eliminate spikes
-% AF2_norm = AF2/AF2_max;
-% 
-% figure()
-% plot(theta, (AF2_norm),theta,y,'-.', theta, y1,'-.',theta,y2,'-.');
-% 
-% figure()
-% plot(theta, db(AF2_norm,'power'),theta,y,'-.', theta, y1,'-.',theta,y2,'-.');
-% 
-% AF2_norm_reduced = AF2_norm;
-% AF2_norm_reduced(db(AF2_norm_reduced,'p') < (rlim_low_db - 2)) = NaN; %remove nulls before polar plot
-% figure()
-% polarplot(deg2rad(theta), db(AF2_norm_reduced,'power'));
-% rlim([rlim_low_db 0])
-% 
-% %display HPBW
-% for i = 1:numel(AF2_norm)
-%     if AF2_norm(i) <= 0.5
-%         disp(2*theta(i))
-%         break
-%     end
-% end
+figure()
+patternCustom(abs(AF_norm), rad2deg(theta), rad2deg(phi), 'CoordinateSystem', 'polar')
