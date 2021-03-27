519e041cd3acf9937f0a27b621f61bdf89b7817e
SyrusVir <jude.alnas@gmail.com>
2021-03-27 17:30:39 -0500

    Added routines for exporting figure 1 and 3 to SVG

--- Array_2D_Factor_Positions.m
+++ Array_2D_Factor_Positions.m
@@ -1,5 +1,5 @@
 close all
-
+set(groot, 'DefaultAxesTickLabelInterpreter', 'latex')
 %%
 c = physconst('LightSpeed');
 Nx = 5; %# of elements in x-direction
@@ -26,13 +26,13 @@ phi = linspace(0,2*pi,500);
 % Dy: size = [Nx Ny]; The value in the ith row, jth column
 %            is the distance in y from the x-axis of the element
 %            in the corresponding row & column 
-d = 1*lambda;   %uniform element spacing in meters
+d = 1*lambda*1.21;   %uniform element spacing in meters
 D = (0:1:Nx-1)*d;   %array of element positions
 [Dx, Dy] = meshgrid(D);
 
 % scanning angle
-scan_theta = deg2rad(0); %scan angle of phased array
-scan_phi   = deg2rad(-50); %scan angle of phased array
+scan_theta = deg2rad(35); %scan angle of phased array
+scan_phi   = deg2rad(75); %scan angle of phased array
 
 %Cartesian axes
 x_cart = sin(Theta).*cos(Phi);
@@ -46,6 +46,10 @@ Beta_y = -k*Dy*sin(scan_theta)*sin(scan_phi);
 
 %smallest dB value shown on rad patter; used to remove extreme nulls from data 
 rlim_low_db = -40; 
+
+% name of svg
+svg_cart_name = sprintf("Figs/%dx%d_%.2f_theta%.1f_phi%.1f.svg",Nx,Ny, d/lambda, rad2deg(scan_theta), rad2deg(scan_phi));
+svg_sph_name = sprintf("Figs/sph%dx%d_%.2f_theta%.1f_phi%.1f.svg",Nx,Ny, d/lambda, rad2deg(scan_theta), rad2deg(scan_phi));
 %% Calculate AF
 AF = zeros(size(x_cart));
 
@@ -69,32 +73,49 @@ AF2 = abs(AF).^2;
 AF2_norm = AF2./AF2_max;
 
 %% Peak Detection
-input = abs(AF2_norm); % select which array factor form to process
+input = abs(AF_norm); % select which array factor form to process
 
-% obtaining all peaks and their indices
+% obtaining all local peaks and their indices
 TF = imregionalmax(input);
-[ind_max_row, ind_max_col] = find(TF);
-ind_peaks = [ind1, ind2];
+[ind_peaks_row, ind_peaks_col] = find(TF);
+ind_peaks = [ind_peaks_row, ind_peaks_col];
 
 % removing the global peak
 peaks = input(TF);
-subpeaks = find(peaks(peaks < max(peaks));
-[SLL, ind_SLL]= max(peaks(peaks ~= input_max));
-ind_SLL = [ind_max_input(ind_SLL,1) ind_max_input(ind_SLL,2)];
+[max_input, ind_max_input] = max(peaks);
+ind_max_input;
+subpeaks = find(peaks(peaks < max(peaks)));
+
 %% Figure 1 plots spatially wihtin unit circle
-figure() 
+
+figure1 = figure();
+array_str = sprintf("%d \\times %d",Nx,Ny);
+theta_str = sprintf("%s = %.1f^{\\circ}", "\theta_0", rad2deg(scan_theta));
+phi_str = sprintf("%s = %.1f^{\\circ}", "\phi_0", rad2deg(scan_phi));
+lambda_str = sprintf("d = %.2f\\lambda", d/lambda);
+
 z_cart = input;
 s1 = surf(x_cart, y_cart,z_cart);
 s1.EdgeColor = 'none';
+title("|AF|^2")
+xlabel('X', 'interpreter', 'latex')
+ylabel("Y",'interpreter', 'latex')
+annotation(figure1,'textbox',...
+    [0.0350629342862732 0.1013015873015876 0.151785714285714 0.137301587301593],...
+    'String',{array_str, lambda_str, theta_str, phi_str},...
+    'FontSize',10,...
+    'FitBoxToText','off',...
+    'EdgeColor','none');
 hold
 plot3(x_cart(TF), y_cart(TF), z_cart(TF), 'r+'); % plotting peak locations determined by imregionmax(0
-plot3(x_cart(ind_max_input(1),ind_max_input(2)),...
-      y_cart(ind_max_input(1),ind_max_input(2)),... 
-      z_cart(ind_max_input(1),ind_max_input(2)), 'gs'); % plotting the max location
-plot3(x_cart(ind_SLL(1),ind_SLL(2)),...
-      y_cart(ind_SLL(1),ind_SLL(2)),...
-      z_cart(ind_SLL(1),ind_SLL(2)), 'bp'); % plotting second highest peaks
-  hold
+% plot3(x_cart(ind_max_input(1),ind_max_input(2)),...
+%       y_cart(ind_max_input(1),ind_max_input(2)),... 
+%       z_cart(ind_max_input(1),ind_max_input(2)), 'gs'); % plotting the max location
+% plot3(x_cart(ind_SLL(1),ind_SLL(2)),...
+%       y_cart(ind_SLL(1),ind_SLL(2)),...
+%       z_cart(ind_SLL(1),ind_SLL(2)), 'bp'); % plotting second highest peaks
+ hold
+ saveas(figure1, svg_cart_name);
 %% Figure 2 plots in cylindrical coordinates 
 % Radius maps to elevation scanning angle
 % Azimuthal angle maps to aimuthal scanning angle
@@ -122,5 +143,13 @@ ylabel('Phi=$\pi/2$ Plane', 'Interpreter', 'latex')
 % % zlim([0 1])
 % s3.EdgeColor = 'none';
 
-figure()
-patternCustom(input, rad2deg(theta), rad2deg(phi), 'CoordinateSystem', 'polar')
+figure3 = figure();
+title("|AF|^2")
+patternCustom(input, rad2deg(theta), rad2deg(phi), 'CoordinateSystem', 'polar');
+annotation(figure3,'textbox',...
+    [0.0350629342862732 0.1013015873015876 0.151785714285714 0.137301587301593],...
+    'String',{array_str, lambda_str, theta_str, phi_str},...
+    'FontSize',10,...
+    'FitBoxToText','off',...
+    'EdgeColor','none');
+saveas(figure3, svg_sph_name);
